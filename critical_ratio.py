6f4fd04ab79512707316ae9ee8ed0be8c29f76d3
dcjenkin1 <dcjenkin@ucsc.edu>
2020-06-17 11:14:30 -0700

    changed paths to save utilization and interarrival csv to persistant volume

--- critical_ratio.py
+++ critical_ratio.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from itertools import chain
 import json
 
-sim_time = 5e5
+sim_time = 5e3
 WEEK = 24*7
 NO_OF_WEEKS = math.ceil(sim_time/WEEK)
 # num_seq_steps = 200
@@ -291,7 +291,7 @@ cols = [mean_util, mean_inter, std_inter, coeff_var, mean_station_takt_times, ma
 df = pd.DataFrame(cols, index=['mean_utilization', 'mean_interarrival_time', 'standard_dev_interarrival',
                   'coefficient_of_var_interarrival', 'mean_station_service_times', 'machines_per_station', 'mean_wait_time'])
 df = df.transpose()
-df.to_csv('util_inter_arr.csv')
+df.to_csv('/persistvol/util_inter_arr.csv')
 # print(df)
 
 # # Plot the time taken to complete each wafer
