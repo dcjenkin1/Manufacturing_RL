236a3b0c06133c084dad6fb33accf6f6644c0e73
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-08-18 14:21:08 +0200

    Update factory_sim.py

--- factory_sim.py
+++ factory_sim.py
@@ -50,7 +50,6 @@ class Machine(object):
     def time_to_failure(self):
         """Return time until next failure for a machine."""
         x = random.expovariate(1/self.break_mean)
-        print(x)
         return x#random.expovariate(1/self.break_mean)
 
     def time_to_repair(self):
