e8ef04f5d1d3a8e74b0b2a00548fc822ad87f468
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-08-21 08:05:21 +0200

    Update PDQN.py

--- PDQN.py
+++ PDQN.py
@@ -10,6 +10,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from keras.regularizers import l2
 
 
 ########################################################################################################################################
