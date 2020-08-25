49c4cbcfb81699425e298706a65546e199a99324
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-08-25 14:43:32 +0200

    run tests in parallel

--- rollout_PDQN.py
+++ rollout_PDQN.py
@@ -346,7 +346,7 @@ machines_per_station = {station: len([mach for mach in my_sim.machines_list if m
                         my_sim.stations}
 
 path,file=os.path.split(args.model_dir)
-data_dir = './'+path+'/data/'
+data_dir = './'+path+'/data/seed_'+seed+'/'+str(id)+'/'
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 
