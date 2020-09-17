43e9d7ff4795afd9a65eabac7538331fa74f4c2c
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2020-09-17 20:46:41 +0200

    Added Rainbow
    Implemented so far:
    Double DQN - YES
    Dueling DQN - YES
    PER - YES
    Multi-step - NO
    Distributional - NO (Might be discarded due to bad performance)
    Noisy Nets - NO (Consider using Boltzman exploration instead)

---
+++ rainbow.py
@@ -0,0 +1,396 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 10:38:37 2020
+
+@author: RTS
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+import numpy as np
+import random
+
+from baselines.common.segment_tree import SumSegmentTree, MinSegmentTree
+from baselines.common.schedules import LinearSchedule
+# Implemented so far:
+    
+# Double DQN - YES
+# Dueling DQN - YES
+# PER - YES
+# Multi-step - NO
+# Distributional - NO (Might be discarded due to bad performance)
+# Noisy Nets - NO (Consider using Boltzman exploration instead)
+
+
+########################################################################################################################################
+#################################################################### CREATING Rainbow Class ####################################
+########################################################################################################################################
+
+class Rainbow:
+    def __init__(self, state_space_dim, action_space, prioritized_replay_beta_iters=None, gamma=0.9, epsilon_decay=0.8, tau=0.125, learning_rate=0.005, epsilon_max=1, batch_size=32, epsilon_min = 0., seed=None):
+        self.state_space_dim = state_space_dim
+        self.action_space = action_space
+        self.gamma = gamma
+        self.epsilon = epsilon_max
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        
+        self.tau = tau
+        self.learning_rate = learning_rate
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        self.batch_size = batch_size
+        self.random_epsilon = random.Random()
+        if seed is not None:
+            self.random_epsilon.seed(seed)
+        self.prioritized_replay_alpha=0.6
+        self.prioritized_replay_beta0=0.4
+        self.prioritized_replay_beta_iters=prioritized_replay_beta_iters
+        self.prioritized_replay_eps=1e-6
+        
+        self.beta_schedule = LinearSchedule(self.prioritized_replay_beta_iters,
+                                       initial_p=self.prioritized_replay_beta0,
+                                       final_p=1.0)
+        
+        self.memory = PrioritizedReplayBuffer(size = 10000, seed=seed, alpha=self.prioritized_replay_alpha)
+
+    # Create the neural network model to train the q function
+    def create_model(self):
+        model_input = keras.Input(shape=(self.state_space_dim))
+        x = layers.Dense(400, activation='relu')(model_input)
+        x = layers.Dense(250, activation='relu')(x)
+        val = layers.Dense(125, activation='relu')(x)
+        val = layers.Dense(1)(val)
+        adv = layers.Dense(125, activation='relu')(x)
+        adv = layers.Dense(len(self.action_space))(adv)
+        
+        reduce_mean = layers.Lambda(lambda w: tf.reduce_mean(w, axis=1, keepdims=True))
+        
+        q_vals = layers.Add()([val, layers.Subtract()([adv, reduce_mean(adv)])])
+        
+        model = keras.models.Model(inputs=model_input , outputs=q_vals)
+        model.compile(optimizer=Adam(lr=self.learning_rate), loss=tf.keras.losses.Huber())
+        return model
+
+    # Action function to choose the best action given the q-function if not exploring based on epsilon
+    def choose_action(self, state, allowed_actions):
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        r = self.random_epsilon.random()
+        if r < self.epsilon:
+            # print("******* CHOOSING A RANDOM ACTION *******")
+            return self.random_epsilon.choice(allowed_actions)
+        # print(state)
+        # print(len(state))
+        state = np.array(state).reshape(1, self.state_space_dim)
+        pred = self.model.predict(state)
+        pred = sum(pred.tolist(), [])
+        temp = []
+        for item in allowed_actions:
+            temp.append(pred[self.action_space.index(item)])
+        # print(" ********************* CHOOSING A PREDICTED ACTION **********************")
+        return allowed_actions[np.argmax(temp)]
+    
+    # Action function to choose the best action given the q-function if not exploring based on epsilon
+    def calculate_value_of_action(self, state, allowed_actions):
+        state = np.array(state).reshape(1, self.state_space_dim)
+        pred = self.model.predict(state)
+        pred = sum(pred.tolist(), [])
+        temp = []
+        for item in allowed_actions:
+            temp.append(pred[self.action_space.index(item)])
+        # print(" ********************* CHOOSING A PREDICTED ACTION **********************")
+        return np.max(temp)
+
+    # Create replay buffer memory to sample randomly
+    def remember(self, state, action, reward, next_state, next_allowed_actions):
+        self.memory.add(state,action,reward,next_state,next_allowed_actions, False)
+
+    # Build the replay buffer # Train the model
+    def replay(self, t, extern_target_model = None):        
+        if len(self.memory) < self.batch_size:
+            return
+        
+        if extern_target_model:
+            target_model = extern_target_model
+        else: 
+            target_model = self.target_model
+            
+        samples = self.memory.sample(self.batch_size, beta=self.beta_schedule.value(t))
+        (states, actions, rewards, new_states, new_allowed_actions, dones, weights, batch_idxes) = samples
+        states = np.array(states).reshape(self.batch_size, self.state_space_dim)
+        preds = self.model.predict(states)
+        target_preds = preds
+
+        action_ids = [self.action_space.index(tuple(action)) for action in actions]
+        # if done:
+        #     target[0][action_id] = reward
+        # else:
+            # take max only from next_allowed_actions
+        new_states = np.array(new_states).reshape(self.batch_size, self.state_space_dim)
+        if extern_target_model:
+            _, next_preds = target_model.predict(new_states) #using lambda predictions
+        else:
+            next_preds_act = self.model.predict(new_states)
+            next_preds = target_model.predict(new_states)
+            
+        # next_preds = next_preds.tolist()
+        # print("new_allowed_actions:", new_allowed_actions)
+        td_errors = []
+        # print(self.action_space)
+        for b in range(self.batch_size):
+            t_act = []
+            t = []
+            if extern_target_model:
+                next_target = next_preds[b]
+            else:
+                for it in new_allowed_actions[b]:
+                    t_act.append(next_preds_act[b][self.action_space.index(it)])
+                    t.append(next_preds[b][self.action_space.index(it)])
+                next_target = t[np.argmax(t_act)]
+            
+            target_preds[b][action_ids[b]] = rewards[b] + self.gamma * next_target
+            td_errors.append(preds[b][action_ids[b]] - target_preds[b][action_ids[b]])
+        loss = self.model.train_on_batch(states, target_preds)
+        new_priorities = np.abs(td_errors) + self.prioritized_replay_eps
+        self.memory.update_priorities(batch_idxes, new_priorities)
+        
+        return loss
+
+
+    # Update our target network
+    def train_target(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        self.target_model.set_weights(target_weights)
+
+    # Save our model
+    def save_model(self, fn):
+        self.model.save(fn)
+        
+    # Load model
+    def load_model(self, model_dir):
+        self.model.load_weights(model_dir)
+        
+# class Replay_buffer:
+#     def __init__(self, memory_size = 10000, seed=None):
+#         assert(memory_size > 0)
+#         self.memory = list([])
+#         self.memory_size = memory_size
+#         self.random_generator = random.Random()
+#         if seed is not None:
+#             self.random_generator.seed(seed)
+    
+#     def put(self, data):
+#         if len(self.memory) < self.memory_size:
+#             self.memory.append(data)
+#         else:
+#             while(len(self.memory) >= self.memory_size):
+#                 self.memory.pop(0)
+#             self.memory.append(data)
+    
+#     def get(self, batch_size=1):
+#         if len(self.memory) >= batch_size:
+#             data = random.sample(self.memory, batch_size)
+#         else: 
+#             data = []
+#             print("Data buffer empty")
+#         return data
+    
+#     def get_pop(self, batch_size=1):
+#         data = []
+#         if len(self.memory):
+#             for _ in range(min(len(self.memory),batch_size)):
+#                 data.append(self.memory.pop(self.random_generator.randint(0,len(self.memory)-1)))
+#         else: 
+#             print("Data buffer empty")
+#         return data
+    
+#     def clear(self):
+#         self.memory = list([])
+        
+class ReplayBuffer(object):
+    def __init__(self, size, seed=None):
+        """Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        """
+        self._storage = []
+        self._maxsize = size
+        self._next_idx = 0
+        self.random_sample = random.Random()
+        if seed is not None:
+            self.random_sample.seed(seed)
+
+    def __len__(self):
+        return len(self._storage)
+
+    def add(self, obs_t, action, reward, obs_tp1, next_allowed_action, done):
+        data = (obs_t, action, reward, obs_tp1, next_allowed_action, done)
+
+        if self._next_idx >= len(self._storage):
+            self._storage.append(data)
+        else:
+            self._storage[self._next_idx] = data
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+
+    def _encode_sample(self, idxes):
+        obses_t, actions, rewards, obses_tp1, next_allowed_actions_arr, dones = [], [], [], [], [], []
+        for i in idxes:
+            data = self._storage[i]
+            obs_t, action, reward, obs_tp1, next_allowed_actions, done = data
+            obses_t.append(np.array(obs_t, copy=False))
+            actions.append(action)
+            rewards.append(reward)
+            obses_tp1.append(np.array(obs_tp1, copy=False))
+            next_allowed_actions_arr.append(next_allowed_actions)
+            dones.append(done)
+        return np.array(obses_t), actions, np.array(rewards), np.array(obses_tp1), next_allowed_actions_arr, np.array(dones)
+
+    def sample(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        next_allowed_actions_batch: np.array
+            next set of allowed actions for the next obs
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        """
+        idxes = [self.random_sample.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
+        return self._encode_sample(idxes)
+
+
+class PrioritizedReplayBuffer(ReplayBuffer):
+    def __init__(self, size, seed=None, alpha=0.6):
+        """Create Prioritized Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        alpha: float
+            how much prioritization is used
+            (0 - no prioritization, 1 - full prioritization)
+        See Also
+        --------
+        ReplayBuffer.__init__
+        """
+        super(PrioritizedReplayBuffer, self).__init__(size)
+        assert alpha >= 0
+        self._alpha = alpha
+
+        it_capacity = 1
+        while it_capacity < size:
+            it_capacity *= 2
+
+        self._it_sum = SumSegmentTree(it_capacity)
+        self._it_min = MinSegmentTree(it_capacity)
+        self._max_priority = 1.0
+
+    def add(self, *args, **kwargs):
+        """See ReplayBuffer.store_effect"""
+        idx = self._next_idx
+        super().add(*args, **kwargs)
+        self._it_sum[idx] = self._max_priority ** self._alpha
+        self._it_min[idx] = self._max_priority ** self._alpha
+
+    def _sample_proportional(self, batch_size):
+        res = []
+        p_total = self._it_sum.sum(0, len(self._storage) - 1)
+        every_range_len = p_total / batch_size
+        for i in range(batch_size):
+            mass = self.random_sample.random() * every_range_len + i * every_range_len
+            idx = self._it_sum.find_prefixsum_idx(mass)
+            res.append(idx)
+        return res
+
+    def sample(self, batch_size, beta):
+        """Sample a batch of experiences.
+        compared to ReplayBuffer.sample
+        it also returns importance weights and idxes
+        of sampled experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        beta: float
+            To what degree to use importance weights
+            (0 - no corrections, 1 - full correction)
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        weights: np.array
+            Array of shape (batch_size,) and dtype np.float32
+            denoting importance weight of each sampled transition
+        idxes: np.array
+            Array of shape (batch_size,) and dtype np.int32
+            idexes in buffer of sampled experiences
+        """
+        assert beta > 0
+
+        idxes = self._sample_proportional(batch_size)
+
+        weights = []
+        p_min = self._it_min.min() / self._it_sum.sum()
+        max_weight = (p_min * len(self._storage)) ** (-beta)
+
+        for idx in idxes:
+            p_sample = self._it_sum[idx] / self._it_sum.sum()
+            weight = (p_sample * len(self._storage)) ** (-beta)
+            weights.append(weight / max_weight)
+        weights = np.array(weights)
+        encoded_sample = self._encode_sample(idxes)
+        return tuple(list(encoded_sample) + [weights, idxes])
+
+    def update_priorities(self, idxes, priorities):
+        """Update priorities of sampled transitions.
+        sets priority of transition at index idxes[i] in buffer
+        to priorities[i].
+        Parameters
+        ----------
+        idxes: [int]
+            List of idxes of sampled transitions
+        priorities: [float]
+            List of updated priorities corresponding to
+            transitions at the sampled idxes denoted by
+            variable `idxes`.
+        """
+        assert len(idxes) == len(priorities)
+        for idx, priority in zip(idxes, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self._storage)
+            self._it_sum[idx] = priority ** self._alpha
+            self._it_min[idx] = priority ** self._alpha
+
+            self._max_priority = max(self._max_priority, priority)
---
+++ rainbow_fact.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 10:38:03 2020
+
+@author: RTS
+"""
+
+import factory_sim as fact_sim
+import numpy as np
+import pandas as pd
+import math 
+# import matplotlib
+import random
+# matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from itertools import chain
+import rainbow
+import argparse
+import datetime
+import json
+import os
+
+parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+# parser.add_argument("--predictron_model_dir", default='./Predictron_DQN_3e5_dense_32_base.h5', help="Path to the Predictron model")
+# parser.add_argument("--state_rep_size", default='32', help="Size of the state representation")
+parser.add_argument("--sim_time", default=1e5, type=int, help="Simulation minutes")
+parser.add_argument("--factory_file_dir", default='b20_setup/', help="Path to factory setup files")
+parser.add_argument("--save_dir", default='data/', help="Path save log files in")
+parser.add_argument("--seed", default=0, help="random seed")
+parser.add_argument('--batch_size', default=32, help='batch size for training')
+args = parser.parse_args()
+
+id = '{date:%Y-%m-%d-%H}'.format(date=datetime.datetime.now())
+
+
+# random.seed(args.seed)
+args = parser.parse_args()
+# model_dir = args.save_dir+str(id)+'/models/dqn_'+str(args.seed)
+res_dir = args.save_dir+args.factory_file_dir+'rainbow/'+'/'+str(id)+'/'
+res_path = res_dir+'rainbow_sim_time'+str(args.sim_time)+'batch_size'+str(args.batch_size)+'seed'+str(args.seed)
+# if not os.path.exists(model_dir):
+#     os.makedirs(model_dir)
+
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+
+sim_time = args.sim_time
+
+WEEK = 24*7
+NO_OF_WEEKS = math.ceil(sim_time/WEEK)
+
+with open(args.factory_file_dir+'break_repair_wip.json', 'r') as fp:
+    break_repair_WIP = json.load(fp)
+
+with open(args.factory_file_dir+'machines.json', 'r') as fp:
+    machine_dict = json.load(fp)
+
+with open(args.factory_file_dir+'recipes.json', 'r') as fp:
+    recipes = json.load(fp)
+
+with open(args.factory_file_dir+'due_date_lead.json', 'r') as fp:
+    lead_dict = json.load(fp)
+
+with open(args.factory_file_dir+'part_mix.json', 'r') as fp:
+    part_mix = json.load(fp)
+
+
+####################################################
+########## CREATING THE STATE SPACE  ###############
+####################################################
+def get_state(sim):
+    # Calculate the state space representation.
+    # This returns a list containing the number of` parts in the factory for each combination of head type and sequence
+    # step
+    state_rep = sum([sim.n_HT_seq[HT] for HT in sim.recipes.keys()], [])
+
+    # print(len(state_rep))
+    # b is a one-hot encoded list indicating which machine the next action will correspond to
+    b = np.zeros(len(sim.machines_list))
+    b[sim.machines_list.index(sim.next_machine)] = 1
+    state_rep.extend(b)
+    # Append the due dates list to the state space for making the decision
+    rolling_window = [] # This is the rolling window that will be appended to state space
+    max_length_of_window = math.ceil(max(sim.lead_dict.values()) / (7*24*60)) # Max length of the window to roll
+
+    current_time = sim.env.now # Calculating the current time
+    current_week = math.ceil(current_time / (7*24*60)) #Calculating the current week 
+
+    for key, value in sim.due_wafers.items():
+        rolling_window.append(value[current_week:current_week+max_length_of_window]) #Adding only the values from current week up till the window length
+        buffer_list = [] # This list stores value of previous unfinished wafers count
+        buffer_list.append(sum(value[:current_week]))
+        rolling_window.extend([buffer_list])
+
+    c = sum(rolling_window, [])
+    state_rep.extend(c) # Appending the rolling window to state space
+    return state_rep
+
+
+# Create the factory simulation object
+my_sim = fact_sim.FactorySim(sim_time, machine_dict, recipes, lead_dict, part_mix, break_repair_WIP['n_batch_wip'],
+                             break_mean=break_repair_WIP['break_mean'], repair_mean=break_repair_WIP['repair_mean'], seed=args.seed)
+# start the simulation
+my_sim.start()
+# Retrieve machine object for first action choice
+mach = my_sim.next_machine
+# Save the state and allowed actions at the start for later use in training examples
+state = get_state(my_sim)
+allowed_actions = my_sim.allowed_actions
+# The action space is a list of tuples of the form [('ht1',0), ('ht1',1), ..., ('ht2', 0), ...] indicating the head
+# types and sequence steps for all allowed actions.
+action_space = list(chain.from_iterable(my_sim.station_HT_seq.values()))
+action_size = len(action_space)
+state_size = len(state)
+
+# Creating the rainbow agent
+rainbow_agent = rainbow.Rainbow(state_space_dim= state_size, action_space= action_space, prioritized_replay_beta_iters=int(sim_time), epsilon_decay=0.999, gamma=0.99, batch_size=32, seed=args.seed)
+
+order_count = 0
+step_counter = 0
+while my_sim.env.now < sim_time:
+    action = rainbow_agent.choose_action(state, allowed_actions)
+    
+    wafer_choice = next(wafer for wafer in my_sim.queue_lists[mach.station] if wafer.HT == action[0] and wafer.seq ==
+                        action[1])
+
+    my_sim.run_action(mach, wafer_choice)
+    # Record the machine, state, allowed actions and reward at the new time step
+    next_mach = my_sim.next_machine
+    next_state = get_state(my_sim)
+    next_allowed_actions = my_sim.allowed_actions
+    reward = my_sim.step_reward
+
+    # Save the example for later training
+    rainbow_agent.remember(state, action, reward, next_state, next_allowed_actions)
+
+    if my_sim.order_completed:
+        # After each wafer completed, train the policy network 
+        rainbow_agent.replay(t=my_sim.env.now)
+        order_count += 1
+        if order_count >= 1:
+            # After every 20 processes update the target network and reset the order count
+            rainbow_agent.train_target()
+            order_count = 0
+
+    # Record the information for use again in the next training example
+    mach, allowed_actions, state = next_mach, next_allowed_actions, next_state
+    step_counter += 1
+    if step_counter % 1000 == 0 and step_counter > 1:
+        print(("%.2f" % (100*my_sim.env.now/sim_time))+"% done")
+        print("Mean lateness: ", np.mean(my_sim.lateness))
+
+# Save the trained rainbow policy network
+rainbow_agent.save_model(res_path+'model.h5')
+
+
+#Wafers of each head type
+print("### Wafers of each head type ###")
+
+# print(my_sim.lateness)
+
+# print(my_sim.complete_wafer_dict)
+
+# Total wafers produced
+print("Total wafers produced:", len(my_sim.cycle_time))
+
+# utilization
+operational_times = {mach: mach.total_operational_time for mach in my_sim.machines_list}
+mach_util = {mach: operational_times[mach]/sim_time for mach in my_sim.machines_list}
+mean_util = {station: round(np.mean([mach_util[mach] for mach in my_sim.machines_list if mach.station == station]), 3)
+             for station in my_sim.stations}
+# mean_mach_takt_times = {mach: np.mean(mach.takt_times) for mach in my_sim.machines_list}
+# std_mach_takt_times = {mach: round(np.std(mach.takt_times), 3) for mach in my_sim.machines_list}
+#
+# mean_station_takt_times = {station: round(np.mean([mean_mach_takt_times[mach] for mach in my_sim.machines_list if
+#                                          mach.station == station and not np.isnan(mean_mach_takt_times[mach])]), 3) for
+#                            station in my_sim.stations}
+# mean_station_takt_times = {station: round(1/sum([1/mean_mach_takt_times[mach] for mach in my_sim.machines_list if
+#                                          mach.station == station]), 3) for station in my_sim.stations}
+
+parts_per_station = {station: sum([mach.parts_made for mach in my_sim.machines_list if mach.station == station]) for
+                     station in my_sim.stations}
+
+station_wait_times = {station: np.mean(sum([my_sim.ht_seq_wait[(ht, seq)] for ht, seq in my_sim.station_HT_seq[station]], [])) for
+                      station in my_sim.stations}
+
+# stdev_util = {station: np.std(mach_util)
+
+inter_arrival_times = {station: [t_i_plus_1 - t_i for t_i, t_i_plus_1 in zip(my_sim.arrival_times[station],
+                                                    my_sim.arrival_times[station][1:])] for station in my_sim.stations}
+mean_inter = {station: round(np.mean(inter_ar_ts), 3) for station, inter_ar_ts in inter_arrival_times.items()}
+std_inter = {station: round(np.std(inter_ar_ts), 3) for station, inter_ar_ts in inter_arrival_times.items()}
+coeff_var = {station: round(std_inter[station]/mean_inter[station], 3) for station in my_sim.stations}
+machines_per_station = {station: len([mach for mach in my_sim.machines_list if mach.station == station]) for station in
+                        my_sim.stations}
+
+# print(np.mean(my_sim.lateness[-1000:]))
+
+cols = [mean_util, mean_inter, std_inter, coeff_var, machines_per_station, station_wait_times]
+df = pd.DataFrame(cols, index=['mean_utilization', 'mean_interarrival_time', 'standard_dev_interarrival',
+                  'coefficient_of_var_interarrival', 'machines_per_station', 'mean_wait_time'])
+df = df.transpose()
+df.to_csv(res_path+'util.csv')
+
+np.savetxt(res_path+'lateness.csv', np.array(my_sim.lateness), delimiter=',')
+
+# print(df)
+# with open(s+'lateness'+id+'.txt','w') as f:
+#   f.write('\n'.join(my_sim.lateness))
+
+# # # Plot the time taken to complete each wafer
+# plt.plot(my_sim.lateness)
+# plt.xlabel("Wafers")
+# plt.ylabel("Lateness")
+# plt.title("The amount of time each wafer was late")
+# plt.show()
+# #
+# # Plot the time taken to complete each wafer
+# plt.plot(my_sim.cumulative_reward_list)
+# plt.xlabel("step")
+# plt.ylabel("Cumulative Reward")
+# plt.title("The sum of all rewards up until each time step")
+# plt.show()
+
+
+
+
+
+
+
+
+
