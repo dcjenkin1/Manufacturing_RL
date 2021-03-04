7dcdeb1ee7b07bded15c29f9e0f79ec1e0fef098
Sørensen <Rene.Arendt.Soerensen@beumer.com>
2021-03-04 21:37:52 +0100

    Update DQN_fact.py

--- DQN_fact.py
+++ DQN_fact.py
@@ -1,3 +1,5 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import factory_sim as fact_sim
 import numpy as np
 import pandas as pd
@@ -8,6 +10,7 @@ import random
 import matplotlib.pyplot as plt
 from itertools import chain
 import DeepQNet
+import DoubleDuelingDeepQNet
 import argparse
 import datetime
 import json
@@ -16,11 +19,13 @@ import os
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 # parser.add_argument("--predictron_model_dir", default='./Predictron_DQN_3e5_dense_32_base.h5', help="Path to the Predictron model")
 # parser.add_argument("--state_rep_size", default='32', help="Size of the state representation")
-parser.add_argument("--sim_time", default=1e5, type=int, help="Simulation minutes")
+parser.add_argument("--sim_time", default=2e6, type=int, help="Simulation minutes")
 parser.add_argument("--factory_file_dir", default='r20_setup/', help="Path to factory setup files")
 parser.add_argument("--save_dir", default='data/', help="Path save log files in")
 parser.add_argument("--seed", default=0, help="random seed")
 parser.add_argument('--batch_size', default=32, help='batch size for training')
+parser.add_argument('--train_rate', default=10, help='The number of steps to take between training the network')
+parser.add_argument('--DDDQN', default=False, help='Use Double Dueling DQN')
 args = parser.parse_args()
 
 id = '{date:%Y-%m-%d-%H}'.format(date=datetime.datetime.now())
@@ -107,7 +112,10 @@ action_size = len(action_space)
 state_size = len(state)
 
 # Creating the DQN agent
-dqn_agent = DeepQNet.DQN(state_space_dim= state_size, action_space= action_space, epsilon_decay=0.999, gamma=0.99, batch_size=32, seed=args.seed)
+if args.DDDQN:
+    dqn_agent = DoubleDuelingDeepQNet.DQN(state_space_dim= state_size, action_space= action_space, epsilon_decay=0.999, gamma=0.99, batch_size=32, seed=args.seed)
+else:
+    dqn_agent = DeepQNet.DQN(state_space_dim= state_size, action_space= action_space, epsilon_decay=0.9999, gamma=0.99, batch_size=32, seed=args.seed)
 
 # if args.seed is not None:# Reinitialize factory with seed
 #     random.seed(args.seed)
@@ -129,9 +137,11 @@ dqn_agent = DeepQNet.DQN(state_space_dim= state_size, action_space= action_space
 
 order_count = 0
 step_counter = 0
+loss = []
+eps = [dqn_agent.epsilon]
 while my_sim.env.now < sim_time:
+    
     action = dqn_agent.choose_action(state, allowed_actions)
-
     wafer_choice = next(wafer for wafer in my_sim.queue_lists[mach.station] if wafer.HT == action[0] and wafer.seq ==
                         action[1])
 
@@ -145,11 +155,12 @@ while my_sim.env.now < sim_time:
     # Save the example for later training
     dqn_agent.remember(state, action, reward, next_state, next_allowed_actions)
 
-    if my_sim.order_completed:
-        # After each wafer completed, train the policy network 
-        dqn_agent.replay()
+    # if my_sim.order_completed:
+    #     # After each wafer completed, train the policy network 
+    if step_counter % args.train_rate == 0:
+        loss.append(dqn_agent.replay())
         order_count += 1
-        if order_count >= 1:
+        if order_count >= 100:
             # After every 20 processes update the target network and reset the order count
             dqn_agent.train_target()
             order_count = 0
@@ -158,8 +169,34 @@ while my_sim.env.now < sim_time:
     mach, allowed_actions, state = next_mach, next_allowed_actions, next_state
     step_counter += 1
     if step_counter % 1000 == 0 and step_counter > 1:
+        eps.append(dqn_agent.epsilon)
         print(("%.2f" % (100*my_sim.env.now/sim_time))+"% done")
         print("Mean lateness: ", np.mean(my_sim.lateness))
+        # # Plot the time taken to complete each wafer
+        plt.plot(my_sim.lateness)
+        plt.xlabel("Wafers")
+        plt.ylabel("Lateness")
+        plt.title("The amount of time each wafer was late")
+        plt.show()
+        
+        # # Plot the loss
+        plt.plot(loss)
+        plt.ylabel("Loss")
+        plt.title("Training loss")
+        plt.show()
+        # # Plot epsilon
+        plt.plot(eps)
+        plt.ylabel("Epsilon")
+        plt.title("Training epsilon")
+        plt.show()
+        #
+        # Plot the time taken to complete each wafer
+        plt.plot(my_sim.cumulative_reward_list)
+        plt.xlabel("step")
+        plt.ylabel("Cumulative Reward")
+        plt.title("The sum of all rewards up until each time step")
+        plt.show()
+        plt.pause(0.05)
 
 # Save the trained DQN policy network
 dqn_agent.save_model(res_path+'model.h5')
@@ -219,21 +256,34 @@ np.savetxt(res_path+'lateness.csv', np.array(my_sim.lateness), delimiter=',')
 # with open(s+'lateness'+id+'.txt','w') as f:
 #   f.write('\n'.join(my_sim.lateness))
 
-# # # Plot the time taken to complete each wafer
-# plt.plot(my_sim.lateness)
-# plt.xlabel("Wafers")
-# plt.ylabel("Lateness")
-# plt.title("The amount of time each wafer was late")
-# plt.show()
-# #
 # # Plot the time taken to complete each wafer
-# plt.plot(my_sim.cumulative_reward_list)
-# plt.xlabel("step")
-# plt.ylabel("Cumulative Reward")
-# plt.title("The sum of all rewards up until each time step")
-# plt.show()
+plt.plot(my_sim.lateness,'.')
+plt.xlabel("Wafers")
+plt.ylabel("Lateness")
+plt.title("The amount of time each wafer was late")
+plt.show()
+
+# # Plot the loss
+plt.plot(loss)
+plt.ylabel("Loss")
+plt.title("Training loss")
+plt.show()
+#
+# Plot the time taken to complete each wafer
+plt.plot(my_sim.cumulative_reward_list)
+plt.xlabel("step")
+plt.ylabel("Cumulative Reward")
+plt.title("The sum of all rewards up until each time step")
+plt.show()
 
 
+data = my_sim.lateness[-10000:]
+binwidth = 2
+plt.hist(data,range(int(min(data)), int(max(data) + binwidth), binwidth))#, histtype=u'step', density=True)
+plt.axvline(np.mean(data), color='r', linestyle='dashed', linewidth=1)
+plt.yscale('log')
+# plt.xlim(-10,1500)
+# min_ylim, max_ylim = plt.ylim()
 
 
 
